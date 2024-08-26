6d737b742ea7931289f895844faf9fa8a78e1261
Vansh Bansal <vb.vansh04@gmail.com>
2024-08-26 23:02:03 +0530

    setting up project requirements

--- Ray3D/src/Application.cpp
+++ Ray3D/src/Application.cpp
@@ -1,9 +1,45 @@
+#include <gl/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+
 #include <iostream>
 
 
-int main()
+int main(void)
 {
-	std::cout << "Hello World" << std::endl;
+    GLFWwindow* window;
 
-	return 0;
+    //Initializing GLFW
+    if (!glfwInit())
+        return -1;
+
+    //Creating Windows
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+
+        glfwTerminate();
+    }
+    glfwMakeContextCurrent(window);
+
+    //Initializing GLEW
+    if (glewInit != GLEW_OK)
+        std::cout << "ERROR::Application.cpp: Failed to initialize GLEW" << std::endl;
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
