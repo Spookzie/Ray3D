947a4de1c319e06d77c9ba99c64a8e164784fa5c
Vansh Bansal <vb.vansh04@gmail.com>
2024-08-30 17:40:49 +0530

    drawing a quad

--- Ray3D/src/Application.cpp
+++ Ray3D/src/Application.cpp
@@ -6,14 +6,16 @@
 
 Vertex vertices[] = {
     //Position                          //Color                         //Texcoords
-    glm::vec3( 0.0f,  0.5f,  0.0f),     glm::vec3(1.0f, 0.0f, 0.0f),    glm::vec2(0.0f, 1.0f),      //index = 0
-    glm::vec3(-0.5f, -0.5f,  0.0f),     glm::vec3(0.0f, 1.0f, 0.0f),    glm::vec2(0.0f, 0.0f),      //        1
-    glm::vec3( 0.5f, -0.5f,  0.0f),     glm::vec3(0.0f, 0.0f, 1.0f),    glm::vec2(1.0f, 0.0f)       //        2
+    glm::vec3(-0.5f,  0.5f,  0.0f),     glm::vec3(1.0f, 0.0f, 0.0f),    glm::vec2(0.0f, 1.0f),      //index = 0 |   top left
+    glm::vec3(-0.5f, -0.5f,  0.0f),     glm::vec3(0.0f, 1.0f, 0.0f),    glm::vec2(0.0f, 0.0f),      //        1 |   bottom left
+    glm::vec3( 0.5f, -0.5f,  0.0f),     glm::vec3(0.0f, 0.0f, 1.0f),    glm::vec2(1.0f, 0.0f),      //        2 |   bottom right
+    glm::vec3( 0.5f,  0.5f,  0.0f),     glm::vec3(0.0f, 1.0f, 0.0f),    glm::vec2(1.0f, 0.0f)       //        3 |   top right
 };
 unsigned vertexCount = sizeof(vertices) / sizeof(Vertex);
 
 GLuint indices[] = {
-    0, 1, 2
+    0, 1, 2,    //Bottom Left triangle
+    0, 2, 3     //Top right triangle
 };
 unsigned indexCount = sizeof(indices) / sizeof(GLuint);
 
